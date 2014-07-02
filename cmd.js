34399c07924fa18d3d19a36c7e7a52a3739e4bfe
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-07-02 22:39:26 +0200

    Update cmd.js

--- cmd.js
+++ cmd.js
@@ -14,7 +14,6 @@ var pkg = require("./package.json");
 program
   .version(pkg.version)
   .description(pkg.description)
-  // -i --input TODO
   .option("-i, --input [file]", "The input file (stdin is used if not provided)", function (path) { return fs.createReadStream(path); }, process.stdin)
   .option("-o, --output [file]", "The output file (stdout is used if not provided)", function (path) { return fs.createWriteStream(path); }, process.stdout)
   .option("-s, --server [port]", "Run a server mode of the minifier", function (port) { return parseInt(port, 10); })

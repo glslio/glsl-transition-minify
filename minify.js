712e6507246d5a885f69497e9515d39826ee046d
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-07-14 19:04:36 +0200

    FIXME: glsl-transition-minify don't minify anymore! too much bugs need to fix them before

--- minify.js
+++ minify.js
@@ -1,9 +1,20 @@
-var through = require('through');
 var glslTokenizer = require('glsl-tokenizer');
 var glslParser = require('glsl-parser');
 var glslMin = require('glsl-min-stream');
 var glslDeparser = require('glsl-deparser');
 
+// FIXME FIXME FIXME: since there is bugs in the minifier, we won't be minifying for now... FIXME FIXME FIXME
+module.exports = function echo (input, onError) {
+  if (!onError) {
+    onError = function (e) {
+      throw e;
+    };
+  }
+  return input
+    .on('error', onError);
+};
+
+/*
 module.exports = function minify (input, onError) {
   if (!onError) {
     onError = function (e) {
@@ -11,6 +22,7 @@ module.exports = function minify (input, onError) {
     };
   }
   return input
+    .on('error', onError)
     .pipe(glslTokenizer())
     .on('error', onError)
     .pipe(glslParser())
@@ -20,3 +32,4 @@ module.exports = function minify (input, onError) {
     .pipe(glslDeparser(false))
     .on('error', onError);
 }
+*/
